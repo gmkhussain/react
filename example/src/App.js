6be69ff285c5972369acc95582f06ef8917a05a1
Hussain <gmk.hussain@yahoo.com>
2018-07-03 19:06:32 -0700

    :rocket: Create, export and import NEW components in React

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class TestMessage extends Component{
+	render(){
+		return <h2>{this.props.testMessageContent}</h2>
+	}
 }
 
-export default App;
+export default TestMessage;
--- example/src/index.js
+++ example/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import TestMessage from './App';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<TestMessage testMessageContent="This is my test message" />, document.getElementById('root'));
+//registerServiceWorker();
