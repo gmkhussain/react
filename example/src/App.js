2d52768f7ab66da85e5f5011c09aca6f7ba02c51
Hussain <gmk.hussain@yahoo.com>
2018-07-09 00:45:52 -0700

    :ballot_box_with_check: Inline Styles in React

--- example/src/App.js
+++ example/src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
+const myStyle = {
+  color: 'orange',
+};
+
 class TestMessage extends Component{
+
 	render(){
-		return <div className="box"><h2><small>To: {this.props.messageTo}</small> <br/>{this.props.testMessageContent} </h2></div>
+		return <div className="box">
+			<h2>
+				<small style={myStyle}>To: {this.props.messageTo}</small>
+				<br/>{this.props.testMessageContent}
+				</h2>
+		</div>
 	}
 }
 
