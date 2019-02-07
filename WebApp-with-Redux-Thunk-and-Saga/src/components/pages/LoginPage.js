c63b7364e8d780465201995b7e7e1e98ba8da2ab
Hussain <gmk.hussain@yahoo.com>
2019-02-07 12:36:04 +0500

    :link: react-router setup with 2 pages ( home & login )

---
+++ WebApp-with-Redux-Thunk-and-Saga/src/components/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const LoginPage = () => (
+    <div>
+        <h1>Login Page</h1>
+        <Link to="/">Home</Link>
+    </div>
+)
+
+
+export default LoginPage;
