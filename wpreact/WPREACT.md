3a3139deb2575bc880b53c2e23da9e9d57bdd0ff
Hussain <gmk.hussain@yahoo.com>
2021-09-13 14:14:04 +0500

    WP React init

---
+++ wpreact/WPREACT.md
@@ -0,0 +1,6 @@
+# install
+```npx create-react-app <yourappname>```
+cd wpreact
+ ```npm i axios react-router-dom```
+After complete installtion
+```npm start```
---
+++ wpreact/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>App</h4>      
+    </div>
+  );
+}
+
+export default App;
